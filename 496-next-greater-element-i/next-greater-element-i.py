f79d7bd72860a2d1a084bbf7359e4ea6588d739b
Ankur Chaudhary <ankurchaudhary627@gmail.com>
2025-01-03 20:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (6.62%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,22 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res=[]
-        while nums1:
-            x=nums1.pop(0)
-            print(x)
-            pos=None
-            for i in range(len(nums2)):
-                if nums2[i]==x:
-                    pos=i
-                    break
-            # print("pos1",pos)
-            found=False
-            for i in range(pos+1,len(nums2)):
-                if nums2[i]>x:
-                    res.append(nums2[i])
-                    found=True
-                    break
-            if not found:
-                res.append(-1)
-            # print("@@")
-        return res
+        st=[]
+        res=dict()
+        for i in nums2:
+            res[i]=-1
+        for i in range(len(nums2)):
+            while st and nums2[i]>nums2[st[-1]]:
+                x=st.pop(-1)
+                res[nums2[x]]=nums2[i]
+            st.append(i)
+        print(res)
+        op=[]
+        for i in nums1:
+            op.append(res[i])
+        return op
